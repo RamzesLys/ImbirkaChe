ea9975db355a7afdba3199aa45df421b543732d4
RamzesLys <ramzes311982@gmail.com>
2022-09-27 04:27:21 +0300

    git push

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -58,41 +58,87 @@ prev.addEventListener('click', prevSlide);
 // setInterval(nextSlide, 3500);
 
 /*SLIDES REVIEWS*/
-const right = document.querySelectorAll('.reviews__btn-next'),
-      left = document.querySelectorAll('.reviews__btn-prev'),
-      revSlides = document.querySelectorAll('.reviews__slide');
+
+function shiftLeft() {
+  const boxes = document.querySelectorAll(".reviews__slide");
+  const tmpNode = boxes[0];
+  boxes[0].className = "reviews__slide move-out-from-left";
+
+  setTimeout(function() {
+      if (boxes.length > 5) {
+          tmpNode.classList.add("reviews__slide-hide");
+          boxes[5].className = "reviews__slide move-to-position5-from-left";
+      }
+      boxes[1].className = "reviews__slide move-to-position1-from-left";
+      boxes[2].className = "reviews__slide move-to-position2-from-left";
+      boxes[3].className = "reviews__slide move-to-position3-from-left";
+      boxes[4].className = "reviews__slide move-to-position4-from-left";
+      boxes[0].remove();
+
+      document.querySelector(".reviews__slider-block").appendChild(tmpNode);
+
+  }, 500);
+
+}
+
+function shiftRight() {
+  const boxes = document.querySelectorAll(".reviews__slide");
+  boxes[4].className = "reviews__slide move-out-from-right";
+  setTimeout(function() {
+      const noOfCards = boxes.length;
+      if (noOfCards > 4) {
+          boxes[4].className = "reviews__slide reviews__slide-hide";
+      }
+
+      const tmpNode = boxes[noOfCards - 1];
+      tmpNode.classList.remove("reviews__slide-hide");
+      boxes[noOfCards - 1].remove();
+      let parentObj = document.querySelector(".reviews__slider-block");
+      parentObj.insertBefore(tmpNode, parentObj.firstChild);
+      tmpNode.className = "reviews__slide move-to-position1-from-right";
+      boxes[0].className = "reviews__slide move-to-position2-from-right";
+      boxes[1].className = "reviews__slide move-to-position3-from-right";
+      boxes[2].className = "reviews__slide move-to-position4-from-right";
+      boxes[3].className = "reviews__slide move-to-position5-from-right";
+  }, 500);
+
+}
+
+// const right = document.querySelectorAll('.reviews__btn-next'),
+//       left = document.querySelectorAll('.reviews__btn-prev'),
+//       revSlides = document.querySelectorAll('.reviews__slide');
       
-let revIndex = 0;
+// let revIndex = 0;
 
-const RevActiveSlide = n => {
-  for(revSlide of revSlides) {
-    revSlide.classList.remove('reviews__active');
-  }
-  revSlides[n].classList.add('reviews__active');
-}
+// const RevActiveSlide = n => {
+//   for(revSlide of revSlides) {
+//     revSlide.classList.remove('reviews__active');
+//   }
+//   revSlides[n].classList.add('reviews__active');
+// }
 
-const rightSlide = () => {
-  if (revIndex == revSlides.length - 1) {
-    revIndex = 0;
-    prepareCurrentSlide(revIndex);
-  } else {
-    revIndex++;
-    prepareCurrentSlide(revIndex);
-  }
-}
+// const rightSlide = () => {
+//   if (revIndex == revSlides.length - 1) {
+//     revIndex = 0;
+//     prepareCurrentSlide(revIndex);
+//   } else {
+//     revIndex++;
+//     prepareCurrentSlide(revIndex);
+//   }
+// }
 
-const leftSlide = () => {
-  if(revIndex == 0) {
-      revIndex = revSlides.length - 1
-      prepareCurrentSlide(revIndex);
-  } else {
-    revIndex--;
-    prepareCurrentSlide(revIndex);
-  }
-}
+// const leftSlide = () => {
+//   if(revIndex == 0) {
+//       revIndex = revSlides.length - 1
+//       prepareCurrentSlide(revIndex);
+//   } else {
+//     revIndex--;
+//     prepareCurrentSlide(revIndex);
+//   }
+// }
 
-right.addEventListener('click', rightSlide);
-left.addEventListener('click', leftSlide);
+// right.addEventListener('click', rightSlide);
+// left.addEventListener('click', leftSlide);
 
 
 
